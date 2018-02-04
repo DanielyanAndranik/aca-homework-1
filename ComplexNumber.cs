aad6bb28f1323f658680103028e94d59ed3ac1e4
Danielyan Andranik <danielyandranik@gmail.com>
2018-02-04 23:57:24 +0400

    First Commit

---
+++ ComplexNumber.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace ComplexNumber
+{
+    public class ComplexNumber
+    {
+        private RealPart realPart;
+        private ImaginityPart imaginityPart;
+
+        public ComplexNumber()
+        {
+            this.realPart= new RealPart(0);
+            this.imaginityPart = new ImaginityPart(0);
+        }
+
+        public ComplexNumber(Double realPart, Double imaginityPart)
+        {
+            this.realPart = new RealPart(realPart);
+            this.imaginityPart = new ImaginityPart(imaginityPart);
+        }
+
+        public ComplexNumber add(ComplexNumber number)
+        {
+            return new ComplexNumber(this.realPart.add(number.realPart), this.imaginityPart.add(number.imaginityPart));
+        }
+
+        public ComplexNumber substract(ComplexNumber number)
+        {
+            return new ComplexNumber(this.realPart.substract(number.realPart), this.imaginityPart.substract(number.imaginityPart));
+        }
+        
+        public ComplexNumber multiply(ComplexNumber number)
+        {
+            Double realPart = this.realPart.multiply(number.realPart) + this.imaginityPart.multiply(number.imaginityPart);
+            Double imaginityPart = this.realPart.multiply(number.imaginityPart) + this.imaginityPart.multiply(number.realPart);
+            return new ComplexNumber(realPart, imaginityPart);
+        }
+
+        public ComplexNumber divide(ComplexNumber number)
+        {
+            Double realPart = this.realPart.multiply(number.realPart) - this.imaginityPart.multiply(number.imaginityPart);
+            Double imaginityPart = -this.realPart.multiply(number.imaginityPart) + this.imaginityPart.multiply(number.realPart);
+            Double denominator = number.realPart.getSquare() + number.imaginityPart.getSquare();
+            return new ComplexNumber(realPart / denominator, imaginityPart / denominator);
+        }
+
+        public Double getAbsValue()
+        {
+            return Math.Sqrt(this.realPart.getSquare() + this.imaginityPart.getSquare());
+        }
+
+        public Double getPhase()
+        {
+            return Math.Atan(this.imaginityPart.getValue() / this.realPart.getValue());
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using ComplexNumber;
+
+namespace ComplexNumber
+{
+    public struct RealPart
+    {
+        private Double value;
+
+        public RealPart(Double value)
+        {
+            this.value = value;
+        }
+
+        public Double add(RealPart number)
+        {
+            return this.value + number.value;
+        }
+
+        public Double substract(RealPart number)
+        {
+            return this.value - number.value;
+        }
+
+        public Double multiply(RealPart number)
+        {
+            return this.value * number.value;
+        }
+
+        public Double multiply(ImaginityPart number)
+        {
+            return this.value * number.getValue();
+        }
+
+        public Double getSquare()
+        {
+            return getValue() * getValue();
+        }
+
+        public Double getValue()
+        {
+            return this.value;
+        }
+
+    }
+
+    public struct ImaginityPart
+    {
+        private Double value;
+
+        public ImaginityPart(Double value)
+        {
+            this.value = value;
+        }
+
+        public Double add(ImaginityPart number)
+        {
+            return this.value + number.value;
+        }
+
+        public Double substract(ImaginityPart number)
+        {
+            return this.value - number.value;
+        }
+
+        public Double multiply(ImaginityPart number)
+        {
+            return -this.value * number.getValue();
+        }
+
+        public Double multiply(RealPart number)
+        {
+            return this.value * number.getValue();
+        }
+
+        public Double getSquare()
+        {
+            return getValue() * getValue();
+        }
+
+        public Double getValue()
+        {
+            return this.value;
+        }
+
+    }
+}
