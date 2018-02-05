9c0b500afe35b1638347b7962c88a71a1ad671de
Danielyan Andranik <danielyandranik@gmail.com>
2018-02-05 10:14:11 +0400

    Add comments

--- CustomTypes.cs
+++ CustomTypes.cs
@@ -3,48 +3,47 @@ using ComplexNumber;
 
 namespace ComplexNumber
 {
-    public struct RealPart
+    public struct RealPart //new type for real part of complex number
     {
         private Double value;
 
-        public RealPart(Double value)
+        public RealPart(Double value) //parametrized constructor
         {
             this.value = value;
         }
 
-        public Double add(RealPart number)
+        public Double add(RealPart number) //method to add two real parts
         {
             return this.value + number.value;
         }
 
-        public Double substract(RealPart number)
+        public Double substract(RealPart number) //method to substract two real parts
         {
             return this.value - number.value;
         }
 
-        public Double multiply(RealPart number)
+        public Double multiply(RealPart number)  //method to multiply two real parts
         {
             return this.value * number.value;
         }
 
-        public Double multiply(ImaginityPart number)
+        public Double multiply(ImaginityPart number)//method to multiply a real and an imaginity parts
         {
             return this.value * number.getValue();
         }
 
-        public Double getSquare()
+        public Double getSquare() // return square of the value
         {
             return getValue() * getValue();
         }
 
-        public Double getValue()
+        public Double getValue() // return the value
         {
             return this.value;
         }
-
     }
 
-    public struct ImaginityPart
+    public struct ImaginityPart //new type for imaginity part of complex number
     {
         private Double value;
 
